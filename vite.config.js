32ebff3a7b29443566d98d58db8d344b90c70849
Maiku <mikearthurminoza@gmail.com>
2025-10-24 14:02:35 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,4 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    build: {
-        outDir: "public/build",
-        manifest: true,
-        emptyOutDir: true,
-    },
 });
